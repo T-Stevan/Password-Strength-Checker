a7123fe9ea90d2639d998de30826f654a1509de0
TSM <trevor.stevan@gmail.com>
2026-09-01 14:07:10 +0300

    Added the password hide/unhide feature using Javascript

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let password = document.getElementById(password);
-console.log(password);
+const passwordField = document.getElementById("password");
+
+const eye = document.getElementById('toggle');
+eye.addEventListener("click", toggle);
+
+function toggle(){
+    // Checks if current type is set to password
+    const isPassword = passwordField.type === "password";
+
+    //  Change type to 'text' if it's 'password', otherwise change it back to
+    passwordField.type = isPassword ? 'text' : 'password';
+
+    const eyeIsOpen = document.querySelector(".eye-open");
+    const eyeIsClosed = document.querySelector(".eye-closed");
+
+    eyeIsOpen.classList.toggle('hidden', isPassword);
+    eyeIsClosed.classList.toggle('hidden', !isPassword);
+}
