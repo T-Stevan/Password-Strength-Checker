8a670b54fe9a47b35886b5d478b5094970cbcb4e
TSM <trevor.stevan@gmail.com>
2026-09-04 13:06:34 +0300

    Implementing password conditions using javascript

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ function toggle(){
     eyeIsOpen.classList.toggle('hidden', isPassword);
     eyeIsClosed.classList.toggle('hidden', !isPassword);
 }
+
+// const noOfChars = document.getElementById("noOfChars");
+// const specialChar = document.getElementById("specialChar");
+// const upperCase = document.getElementById("upperCase");
+// const lowerCase = document.getElementById("lowerCase");
+// const numbers = document.getElementById("numbers");
+
+
+passwordField.addEventListener("input", function(){
+    const passwordValue = passwordField.value;
+    const currentLength = passwordValue.length;
+    const hasNumber = /\d/.test(passwordValue); //checks for numbers in value
+
+    const finalStength = document.getElementById("finalStrength");
+    if (currentLength >= 8 && hasNumber){ // still modifying the conditions
+        finalStength.innerText = "Strong password";
+        finalStength.style.color = "#20f907";
+    } 
+    else {
+        finalStength.innerText = "Weak password";
+        finalStength.style.color = "yellow";
+    }
+})
+
